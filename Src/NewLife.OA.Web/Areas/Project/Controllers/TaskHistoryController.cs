64d8d72d2900e09e3d8210a4f89340c2a1d56e12
nnhy <nnhy@vip.qq.com>
2015-05-29 08:37:03 +0000

    增加任务历史和任务评论控制器，成功指向

---
+++ Src/NewLife.OA.Web/Areas/Project/Controllers/TaskHistoryController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NewLife.Cube;
+using NewLife.Web;
+
+namespace NewLife.OA.Web.Areas.Project.Controllers
+{
+    public class TaskHistoryController : EntityController<TaskHistory>
+    {
+        protected override ActionResult IndexView(Pager p)
+        {
+            var id = RouteData.Values["id"].ToInt();
+            var list = TaskHistory.Search(id, p);
+
+            return View("List", list);
+        }
+    }
+}
