2a390cc63de4f5806115d85b491a2f5fd6c56f0e
nnhy <nnhy@vip.qq.com>
2015-06-30 14:11:24 +0000

    增加版本

--- Src/NewLife.OA.Web/Properties/AssemblyInfo.cs
+++ Src/NewLife.OA.Web/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息通过下列特性集
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("NewLife.OA.Web")]
+[assembly: AssemblyTitle("新生命办公自动化系统")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("新生命开发团队")]
 [assembly: AssemblyProduct("NewLife.OA.Web")]
-[assembly: AssemblyCopyright("版权所有(C) Microsoft 2015")]
+[assembly: AssemblyCopyright("\x00a92002-2015 新生命开发团队")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // 可以指定所有这些值，也可以使用“修订号”和“内部版本号”的默认值，
 // 方法是按如下所示使用“*”:
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
